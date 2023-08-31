986c8eac77dd693f9bc5d85f2e9f6c2afd132b69
uwinnaa <winanna675@gmail.com>
2023-08-31 14:04:27 +0700

    ada tabelcu

---
+++ views/table.php
@@ -0,0 +1,43 @@
+<?php
+   include_once 'template/header.php';
+   include_once 'template/sidebar.php';
+   include_once 'template/topbar.php';
+
+
+?>
+<div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+</tbody>
+</table>
+</div>
+</div>
+<?php
+    //modular untuk memanggil file dari folder template
+    include_once 'template/footer.php';
+?>
